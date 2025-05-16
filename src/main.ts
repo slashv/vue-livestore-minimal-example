914667ab4bf291a9f3141575af6b2f724f624093
Victor Nyberg <contact@victornyberg.com>
2025-05-16 11:14:26 +1000

    Cleaned up irrelevant files

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,11 @@ import { storePromise, StoreKey } from './store'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
 const store = await storePromise
 
 const app = createApp(App)
 
 app.provide(StoreKey, store)
-app.use(router)
 
 app.mount('#app')
